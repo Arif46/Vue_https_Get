4321a7fdb8acde5ad9f25d0944064a36f031a4df
Arif.Trinoq <asarifkhan08@gmail.com>
2020-11-19 09:45:11 -0600

    vue js project initial

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+<Blog/>
+</template>
+<script>
+ import Blog from './components/Blog'
+ export default {
+     data(){
+
+     },
+     components : {
+         Blog
+     }
+ }
+ </script>
---
+++ src/components/Blog.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container">
+        this is blog
+            <div v-for="(blog ,id) in blogs" :key="id">
+                <h1>{{blog.Id}}</h1>
+                    <h3>{{blog.userId}}</h3>
+                    <h3>{{blog.title}}</h3>
+                    <h5>{{blog.body}}</h5>
+            </div>
+    </div>
+</template>
+<script>
+   export default {
+       data(){
+           return {
+                blogs:[]
+           }
+       },
+       created(){
+           this.$http.get('https://jsonplaceholder.typicode.com/posts')
+           .then((data)=>{
+               console.log(data);
+               this.blogs=data.body.slice(0,10)
+           })
+       }
+   }
+</script>
+<style>
+   div{
+       color:red
+   }
+</style>
